a10a346e417fa4dce13998fa73449a0848678192
cuimingqiang <770460131@qq.com>
2015-07-21 17:03:27 +0800

    Initial commit

---
+++ app/src/main/java/com/cmq/bus/MessageEvent.java
@@ -0,0 +1,7 @@
+package com.cmq.bus;
+
+/**
+ * Created by liqiang on 2015/7/21.
+ */
+public class MessageEvent {
+}
---
+++ app/src/main/java/com/cmq/bus/eventBus/EventBusActivity.java
@@ -0,0 +1,61 @@
+package com.cmq.bus.eventBus;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cmq.bus.MessageEvent;
+import com.cmq.bus.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import de.greenrobot.event.EventBus;
+
+/**
+ * Created by liqiang on 2015/7/21.
+ */
+public class EventBusActivity extends Activity{
+    private EventBus eventBus;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eventbus);
+        ButterKnife.bind(this);
+        eventBus = EventBus.getDefault();
+        eventBus.register(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        eventBus.unregister(this);
+    }
+    @OnClick(R.id.button)
+    public void onClick(View view){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                eventBus.post(new MessageEvent());
+            }
+        }).start();
+
+    }
+
+
+    // Called in the same thread (default)
+    public void onEvent(MessageEvent event) {
+        System.out.println("onEvent---"+Thread.currentThread().getName());
+    }
+    // Called in Android UI's main thread
+    public void onEventMainThread(MessageEvent event) {
+        System.out.println("onEventMainThread---" + Thread.currentThread().getName());
+    }
+    // Called in the background thread
+    public void onEventBackgroundThread(MessageEvent event){
+        System.out.println("onEventBackgroundThread---" + Thread.currentThread().getName());
+    }
+    // Called in a separate thread
+    public void onEventAsync(MessageEvent event){
+        System.out.println("onEventAsync---" + Thread.currentThread().getName());
+    }
+}
